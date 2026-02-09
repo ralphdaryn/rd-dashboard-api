db724f79aa81bc3fb3031cfa1de90b5057920c03
ralphdaryn <ralphdarync@gmail.com>
2026-02-08 20:14:36 -0500

    add cors in controller

--- src/main/java/ca/rddigitech/rd_dashboard_api/ApiController.java
+++ src/main/java/ca/rddigitech/rd_dashboard_api/ApiController.java
@@ -1,12 +1,17 @@
 package ca.rddigitech.rd_dashboard_api;
 
 import java.util.Map;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8888")
+@CrossOrigin(origins = {
+  "http://localhost:8888",
+  "https://stepbystepclub.ca",
+  "https://www.stepbystepclub.ca"
+})
 @RestController
 @RequestMapping("/api")
 public class ApiController {
